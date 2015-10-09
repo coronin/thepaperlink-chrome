61cb936a5d1e6ad6e9d74e598ec1ca1e303224c1
Liang C <liang.cai@me.com>
2015-10-09 16:21:11 -0700

    leaving SCE

--- options.js
+++ options.js
@@ -58,7 +58,9 @@ function reset_key(v) {
 }
 
 function valid_thepaperlink(ak) {
-  return $.get('http://www.zhaowenxian.com/api?validate=' + ak,
+  return $.get('http://www.zhaowenxian.com/api',
+      { validate: ak,
+         runtime: chrome.runtime.id },
       function (txt) {
         if (txt === 'valid') {
           _port.postMessage({save_apikey: ak, save_email: null});
