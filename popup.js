1882831829d1c65aa03f9bf1684b1a22707730a0
Liang Cai <coronin@gmail.com>
2018-09-20 21:29:58 +0800

    popup.js need a fix

--- popup.js
+++ popup.js
@@ -229,21 +229,21 @@ $(document).ready(function () {
 
 chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
   var tab = tabs[0],
-      ID = localStorage.getItem('tabId:' + tab.id.toString()),
       dotCheck = /\./,
-      pmcCheck = /PMC/,
-      url_trim = tab.url.substr(7, 25);
-  if (tab.url.indexOf('chrome-extension://') === 0) {
-    $('#result').html('popup.js used in background.html');
+      pmcCheck = /PMC/;
+  var ID = localStorage.getItem("tabId:" + tab.id.toString());
+  var url_trim = tab.url.substr(7, 25);
+  if (tab.url.indexOf("chrome-extension://") === 0) {
+    $("#result").html("popup.js used in background.html");
   } else {
-    chrome.pageAction.setIcon({path: '19.png', tabId: tab.id});
-    chrome.pageAction.setTitle({title: 'extracted', tabId: tab.id});
+    chrome.pageAction.setIcon({path: "19.png", tabId: tab.id});
+    chrome.pageAction.setTitle({title: "extracted", tabId: tab.id});
     if (tab.url.substr(7, 26) !== url_trim) {
-      url_trim += '&hellip;';
+      url_trim += "&hellip;";
     }
     if (dotCheck.test(ID)) {
-      $('#found').html('DOI:<span class="eSS" id="' + ID + '">' + ID + '</span> found on page ' + url_trim);
-      $('.eSS').on('click', function () { eSS(this.id); });
+      $("#found").html("DOI:<span class=\'"eSS" id="' + ID + '">' + ID + '</span> found on page ' + url_trim);
+      $(".eSS").on('click', function () { eSS(this.id); });
     } else if (pmcCheck.test(ID)) {
       $('#found').html('PMCID:<span class="eSS" id="' + ID + '">' + ID + '</span> found on page ' + url_trim);
       $('.eSS').on('click', function () { eSS(this.id); });
