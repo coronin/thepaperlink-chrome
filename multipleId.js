ffece12d7da40aa523f1fcf5e3c376f9bdd53d1e
Liang Cai <coronin@gmail.com>
2018-09-29 01:09:52 +0800

    better /js inject, late night

--- multipleId.js
+++ multipleId.js
@@ -4,6 +4,7 @@ var js_base = 'http://phd.cail.cn/';
 if (window.location.protocol === 'https:') {
     js_base = 'https://pubget-hrd.appspot.com/';
 }
+
 chrome.storage.sync.get(['GUEST_APIKEY', 'thepaperlink_apikey'], function (e) {
     localStorage.setItem('thePaperLink_pubget_js_key', e.thepaperlink_apikey || e.GUEST_APIKEY);
     localStorage.setItem('thePaperLink_pubget_js_base', js_base);
@@ -12,7 +13,7 @@ chrome.storage.sync.get(['GUEST_APIKEY', 'thepaperlink_apikey'], function (e) {
         var jsClient = document.createElement('script');
         jsClient.setAttribute('type', 'text/javascript');
         jsClient.setAttribute('src', js_base + 'js?y=' + (Math.random()));
-        document.body.appendChild(jsClient); // @@@@ fail to insert or.nsfc
+        (document.head || document.documentElement).appendChild(jsClient);
     }
 });
 
