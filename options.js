52ac7695b658eb1cfed670fd33ba1af6e2c538b6
Liang C <410795+coronin@users.noreply.github.com>
2020-08-25 18:55:43 +0800

    fix: bugs

--- options.js
+++ options.js
@@ -445,7 +445,7 @@ $(document).ready(function () {
   }
   if (cc_address) {
     $('#cc_address').val(cc_address);
-    $('#cc_enabled').text('will get the abstract when you "email it".');
+    $('#cc_enabled').text('will get the abstract when you "email it" (expanded button from ... on the bar).');
   }
   if (localStorage.getItem('alert_outdated')) {
     $('.alert_outdated').removeClass('Off');
